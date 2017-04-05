54d15fa65cd73232939b3077a9724a5dcd868a39
FSEVERI\pagiaro3283 <FSEVERI\pagiaro3283@linf4-11.fseveri.it>
2017-04-05 08:36:52 +0200

    logout

--- src/java/controller/LoginController.java
+++ src/java/controller/LoginController.java
@@ -1,6 +1,10 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Classe LoginController
@@ -9,4 +13,16 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class LoginController {
     
+    /**
+     * Metodo per eseguire il logout
+     * @param map mappa degli attributi
+     * @param request http request
+     * @return il nome della vista
+     */
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(ModelMap map, HttpServletRequest request) {
+        request.getSession().removeAttribute("idUtente");
+        map.addAttribute("idUtente", false);
+        return "redirect:/";
+    }
 }
